a6cb37a349b18e552e82a719e8aeec02ee6d2368
KEVIN D Steele <kevindsteeleii@gmail.com>
2018-07-23 12:21:57 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,3 +6,7 @@ function updatePlaylist(playList, artistName, songTitle) {
   playList[artistName] = songTitle
   return playList
 }
+
+function removeFromPlaylist (playList, artistName) {
+  delete playList[artistName]
+}
